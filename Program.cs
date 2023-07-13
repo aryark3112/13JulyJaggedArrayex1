95414f06cf8695099626014f590cf284b5c748fc
aryark <aryark3112@gmail.com>
2023-07-13 09:53:39 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -1,24 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace _13july
+//namespace _13july
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Conditional Operator (?true case :false case)
+//int num1, num2;
+//Console.WriteLine("Enter First Number");
+//num1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Enter Second Number");
+//num2 = int.Parse(Console.ReadLine());
+//var result = num1 > num2 ? "First Number is greater" : "Second Number is greater";
+//Console.WriteLine(result);
+//Console.ReadKey();
+
+//int num1, num2;
+//Console.WriteLine("Enter First Number");
+//num1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Enter Second Number");
+//num2 = int.Parse(Console.ReadLine());
+//if (num1!= num2)
+//{
+//    var result = num1 > num2 ? "First Number is greater" : "Second Number is greater";
+//    Console.WriteLine(result);
+//}
+//else { Console.WriteLine("Numbers are equal!!");
+//}
+//Console.WriteLine(result);
+//Console.ReadKey();
+
+using System;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Enter marks in Pre:");
+        int preMarks = Convert.ToInt32(Console.ReadLine());
+
+        if (preMarks > 60)
         {
-            //Conditional Operator (?true case :false case)
-            int num1, num2;
-            Console.WriteLine("Enter First Number");
-            num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Second Number");
-            num2 = int.Parse(Console.ReadLine());
-            var result = num1 > num2 ? "First Number is greater" : "Second Number is greater";
-            Console.WriteLine(result);
-            Console.ReadKey();
+            Console.WriteLine("Enter marks in Final:");
+            int finalMarks = Convert.ToInt32(Console.ReadLine());
+
+            if (finalMarks > 55)
+            {
+                Console.WriteLine("Result: Selected");
+            }
+            else
+            {
+                Console.WriteLine("Result: Fail in Final");
+            }
         }
+        else
+        {
+            Console.WriteLine("Result: Fail in Pre");
+        }
+
+        Console.ReadLine();
     }
 }
+
+
+//        }
+//    }
+//}
