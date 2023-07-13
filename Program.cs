e9ececaf7acbdf9a462359e8c8556e4019024bf1
aryark <aryark3112@gmail.com>
2023-07-13 12:01:30 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -111,22 +111,45 @@ namespace _13july
             //}
             //Console.ReadKey();
 
+            //int[,] marks = new int[4, 3];
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Console.WriteLine($"Enter Student {i + 1}\'s Roll Number");
+            //    marks[i, 0] = int.Parse(Console.ReadLine());
+            //    for (int j = 1; j < 3; j++)
+            //    {
+            //        Console.WriteLine($"Enter marks of Student\'s{i + 1} in Sem{j}");
+            //        marks[i, j] = int.Parse(Console.ReadLine());
+            //    }
+            //}
+            //Console.WriteLine("Marks List as Follows");
+            //Console.WriteLine("RollNo\tSemOne\t SemTwo");
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    for (int j = 1; j < 3; j++)
+            //    {
+            //        Console.Write(marks[i, j] + "\t");
+            //    }
+            //    Console.WriteLine("\n");
+            //}
+            //Console.ReadKey();
+
             int[,] marks = new int[4, 3];
-            for (int i=0; i<4; i++)
+            for (int i = 0; i < 4; i++)
             {
-                Console.WriteLine($"Enter Student {i + 1}\'s Roll Number");
+                Console.WriteLine($"Enter Student {i + 1} \'s Roll Number");
                 marks[i, 0] = int.Parse(Console.ReadLine());
-                for(int j=1;j<3;j++)
+                for (int j = 1; j < 3; j++)
                 {
-                    Console.WriteLine($"Enter marks of Student\'s{i + 1} in Sem{j}");
+                    Console.WriteLine($"Enter marks of Student\'s {i + 1} in Sem {j}");
                     marks[i, j] = int.Parse(Console.ReadLine());
                 }
             }
-            Console.WriteLine("Marks List as Follows");
-            Console.WriteLine("RollNo\tSemONE\tSemTwo");
-            for (int i = 0; i < 4; i++)
+            Console.WriteLine("Marks List as follovs");
+            Console.WriteLine("RollNo\t SemOne\t SemTwo");
+            for (int i = 0; 1 < 4; i++)
             {
-                for (int j = 1; j < 3; j++)
+                for (int j = 0; j < 3; j++)
                 {
                     Console.Write(marks[i, j] + "\t");
                 }
